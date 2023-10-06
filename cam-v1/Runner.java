7dc14d051f98b326878bd08226f421d0fd94b449
ollieoko <ollieoko@ahs.local>
2023-10-06 15:05:51 -0700

    w2dfj

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,40 @@
-public class Runner{
-    public static void main(String[] args) {
-        VirtualPet v = new VirtualPet();
 
-        v.exercise();
+ import javax.swing.*;
+public class Runner{
+    public Runner(){
+        VirtualPet p = new VirtualPet();
+
+        p.exercise();
+        takeAbeat(1000);
+        p.feed();
+        takeAbeat(4000);
+        p.exercise();
+        String ans = getResponse("How was your day?");
+        System.out.println(ans);
+       
+
+    }
+
+    public String getResponse(String q){
+        String s = (String)JOptionPane.showInputDialog(
+         new JFrame(),
+         q,
+         "Get a response",
+         JOptionPane.PLAIN_MESSAGE
+        );
+        return s;
+
+    }
+    
+    public void takeAbeat(int millisec){
+          try{
+            Thread.sleep(millisec); 
+        }catch(Exception e){
+        }
+    }
+
+    public static void main(String[] args) {
+        new Runner();
+        
     }
 }
